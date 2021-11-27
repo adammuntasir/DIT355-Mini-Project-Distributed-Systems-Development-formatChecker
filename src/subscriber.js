5a1158cd65591ab4f7783ff25c3f19efd8fc0a7d
sramzi <sramzi@student.chalmers.se>
2021-11-27 13:01:39 +0100

    removed unwanted comments and console logs

--- src/subscriber.js
+++ src/subscriber.js
@@ -13,42 +13,42 @@ const EventEmitter = require("events"); //This is a build in class in Node.js re
 const emitter = new EventEmitter(); //Creating an new emitter that forwards incoming MQTT messages as events
 
 var subscriber = {
-  eventListener: emitter, //This will export our emitter and include it in the module that index.js requires
+    eventListener: emitter, //This will export our emitter and include it in the module that index.js requires
 
-  start: function () {
-    //Called when client is connected
-    client.on("connect", function () {
-      console.log("Status: Subscriber is connected to broker");
-      startSubscription();
-    });
+    start: function() {
+        //Called when client is connected
+        client.on("connect", function() {
+            console.log("Status: Subscriber is connected to broker");
+            startSubscription();
+        });
 
-    //Called when client is disconnected
-    client.on("disconnect", function () {
-      console.log("Status: Subscriber has been disconnected");
-    });
+        //Called when client is disconnected
+        client.on("disconnect", function() {
+            console.log("Status: Subscriber has been disconnected");
+        });
 
-    //Called when client is reconnecting
-    client.on("reconnect", function () {
-      console.log("Status: Subscriber is reconnecting");
-    });
+        //Called when client is reconnecting
+        client.on("reconnect", function() {
+            console.log("Status: Subscriber is reconnecting");
+        });
 
-    //Called when client is offline
-    client.on("offline", function () {
-      console.log("Status: Subscriber is offline");
-      client.reconnect();
-    });
+        //Called when client is offline
+        client.on("offline", function() {
+            console.log("Status: Subscriber is offline");
+            client.reconnect();
+        });
 
-    //Called when client receives a message
-    client.on("message", function (topic, message) {
-      //Here, when our MQTT client recieves a message from the broker, we let our emitter
-      // //forward the topic and message internally to the index.js as an event mqttRecieved
-      emitter.emit("mqttRecieved", topic, message);
-    });
-  },
+        //Called when client receives a message
+        client.on("message", function(topic, message) {
+            //Here, when our MQTT client recieves a message from the broker, we let our emitter
+            // //forward the topic and message internally to the index.js as an event mqttRecieved
+            emitter.emit("mqttRecieved", topic, message);
+        });
+    },
 };
 
 function startSubscription() {
-  client.subscribe(access.FromClient); // receive the chosen date
+    client.subscribe(access.FromClient); // receive the chosen date
 }
 
 module.exports = subscriber;
